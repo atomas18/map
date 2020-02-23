be3658bc9aa244af17afea009e8c8151862bd92d
atomas18 <thomas.flurschuetz@gmail.com>
2020-02-23 15:59:08 +0100

    new style, no areal display

--- scripts/state.js
+++ scripts/state.js
@@ -1,7 +1,7 @@
 function getData_state(){
 	var data = new Map();
 
-    data.set(1896, { name: "Max Mustermann", comment: "bla blub" });
+    data.set(1896, { name: "Max Mustermann", comment: "Pate bis 24.Feb 2020" });
 
 	return data;
 }
