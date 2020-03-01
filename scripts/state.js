61d5a3f63c07f3635f068ec7c374b933dc2ff930
atomas18 <thomas.flurschuetz@googlemail.com>
2020-03-01 10:24:46 +0100

    more state descriptions

--- scripts/state.js
+++ scripts/state.js
@@ -1,7 +1,9 @@
 function getData_state(){
 	var data = new Map();
 
-    data.set(1896, { name: "Max Mustermann", comment: "Pate bis 24.Feb 2020" });
+        data.set(1896, { name: "Max Mustermann", comment: "Pate bis 24.Feb 2020" });
+	data.set(1953, { name: "Beate Beispiel", comment: "Patin bis 1.März 2020" });
+	data.set(1894, { name: "Anonym", comment: "Pate bis 10.Mai 2020" });
 
 	return data;
 }
